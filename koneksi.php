0d3473420362106ad8f0bcb110ba1cca798289d4
Fanani <fanani@bangkit.academy>
2023-11-22 14:39:23 +0700

    modifikasi koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $serverName = "DESKTOP-JSTOPU8";// Nama server SQL Server
 $connectionOptions = array(
-    "Database" => "N1_toko", // Nama database
+    "Database" => "N2_toko", // Nama database
     "Uid" => "",      // Nama pengguna SQL Server
     "PWD" => "",
     "TrustServerCertificate" => "True"        // Kata sandi SQL Server
